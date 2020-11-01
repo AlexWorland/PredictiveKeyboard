a7955f2b11ba45f80ef991eb5b2fb82d5e5aaa28
alexworland <star trek fan1234>
2020-10-31 17:33:02 -0700

    Network functions

--- PredictiveKeyboard.py
+++ PredictiveKeyboard.py
@@ -19,15 +19,21 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    data = getDataset()
+    # Constants
+    numSuggestions = 5
+    # TODO: make user specifiable
+    numPrevWords = 5
+    epochs = 20
+
+    # path = str(input("Please enter the file path of the input dataset: "))
+    path = 'holmes.txt'
+    data = getDataset(path)
     uniqueWords = np.unique(data)
     uniqueWordsIndex = dict((c, i) for i, c in enumerate(uniqueWords))
 
-    # TODO: make user specifiable
-    numPrevWords = 5
     previousWords = []
     nextWords = []
-    for i in range(len(data - numPrevWords)):
+    for i in range(len(data) - numPrevWords):
         previousWords.append(data[i:i + numPrevWords])
         nextWords.append(data[i + numPrevWords])
     print(previousWords[0])
@@ -37,18 +43,18 @@ def main():
     Y = np.zeros((len(nextWords), len(uniqueWords)), dtype=bool)
     for i, eachWords in enumerate(previousWords):
         for j, eachWord in enumerate(eachWords):
-            X[i:j, uniqueWordsIndex[eachWord]] = 1
+            X[i, j, uniqueWordsIndex[eachWord]] = 1
         Y[i, uniqueWordsIndex[nextWords[i]]] = 1
     print(X[0][0])
 
     model = Sequential()
-    model.add(LSTM(128, input_shape=(numPrevWords, len(previousWords))))
+    model.add(LSTM(128, input_shape=(numPrevWords, len(uniqueWords))))
     model.add(Dense(len(uniqueWords)))
     model.add(Activation('softmax'))
 
     optimizer = RMSprop(lr=0.01)
     model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-    history = model.fit(X, Y, validation_split=0.05, batch_size=128, epochs=2, shuffle=True).history
+    history = model.fit(X, Y, validation_split=0.05, batch_size=128, epochs=epochs, shuffle=True).history
 
     model.save('kerasNextWordModel.h5')
     pickle.dump(history, open("history.p", "wb"))
@@ -59,16 +65,15 @@ def main():
     print("Correct Sentence: ", q)
     seq = " ".join(RegexpTokenizer(r'\w+').tokenize(q.lower())[0:numPrevWords])
     print("Sequence: ", seq)
-    print("next possible words: ", predictCompletions(seq, model, uniqueWords))
+    print("next possible words: ", predictCompletions(seq, model, uniqueWords, uniqueWordsIndex, numPrevWords, numSuggestions))
 
 
-def getDataset():
+def getDataset(path):
     """
     A function that returns the text from an input file
     :return: Returns the text from an input file
     """
-    path = input("Please enter the file path of the input dataset: ")
-    text = open(path).read().lower()
+    text = open(path, encoding='utf8').read().lower()
     print('corpus length: ', len(text))
     return splitDataset(text)
 
@@ -92,7 +97,7 @@ def prepareInput(text, numPreviousWords, uniqueWords, uniqueWordsIndex):
     return X
 
 
-def sample(preds, top_n=3):
+def sample(preds, top_n):
     preds = np.asarray(preds).astype('float64')
     preds = np.log(preds)
     expPreds = np.exp(preds)
@@ -100,10 +105,17 @@ def sample(preds, top_n=3):
     return heapq.nlargest(top_n, range(len(preds)), preds.take)
 
 
-def predictCompletions(text, model, uniqueWords, n=3):
+def predictCompletions(text, model, uniqueWords, uniqueWordsIndex, numPreviousWords, n):
     if text == "":
         return 0
-    x = prepareInput(text)
+    x = prepareInput(text, numPreviousWords, uniqueWords, uniqueWordsIndex)
     preds = model.predict(x, verbose=0)[0]
     next_indicies = sample(preds, n)
     return [uniqueWords[idx] for idx in next_indicies]
+
+
+if __name__ == '__main__':
+    import tensorflow as tf
+
+    with tf.device("/device:gpu:0"):
+        main()
