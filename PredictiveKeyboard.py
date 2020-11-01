0d3e3d7775b2676fc9a073a6c6069b0804eae680
alexworland <alexworland@gmail.com>
2020-10-31 18:19:43 -0700

    Major refactoring

--- PredictiveKeyboard.py
+++ PredictiveKeyboard.py
@@ -15,7 +15,9 @@ from keras.layers.core import Dense, Activation
 from keras.optimizers import RMSprop
 import pickle
 import heapq
-import matplotlib.pyplot as plt
+
+
+# import matplotlib.pyplot as plt
 
 
 def main():
@@ -24,48 +26,35 @@ def main():
     # TODO: make user specifiable
     numPrevWords = 5
     epochs = 20
+    optimizer = RMSprop(lr=0.01)
 
     # path = str(input("Please enter the file path of the input dataset: "))
-    path = 'holmes.txt'
+    path = 'homes.txt'
     data = getDataset(path)
     uniqueWords = np.unique(data)
     uniqueWordsIndex = dict((c, i) for i, c in enumerate(uniqueWords))
 
-    previousWords = []
-    nextWords = []
-    for i in range(len(data) - numPrevWords):
-        previousWords.append(data[i:i + numPrevWords])
-        nextWords.append(data[i + numPrevWords])
-    print(previousWords[0])
-    print(nextWords[0])
+    previousWords = preparePrevWords(data, numPrevWords)
+    nextWords = prepareNextWords(data, numPrevWords)
 
-    X = np.zeros((len(previousWords), numPrevWords, len(uniqueWords)), dtype=bool)
-    Y = np.zeros((len(nextWords), len(uniqueWords)), dtype=bool)
-    for i, eachWords in enumerate(previousWords):
-        for j, eachWord in enumerate(eachWords):
-            X[i, j, uniqueWordsIndex[eachWord]] = 1
-        Y[i, uniqueWordsIndex[nextWords[i]]] = 1
-    print(X[0][0])
+    X, Y = prepareXYDatasets(previousWords, nextWords, numPrevWords, uniqueWords, uniqueWordsIndex)
 
-    model = Sequential()
-    model.add(LSTM(128, input_shape=(numPrevWords, len(uniqueWords))))
-    model.add(Dense(len(uniqueWords)))
-    model.add(Activation('softmax'))
+    model = prepareModel(numPrevWords, uniqueWords)
 
-    optimizer = RMSprop(lr=0.01)
-    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-    history = model.fit(X, Y, validation_split=0.05, batch_size=128, epochs=epochs, shuffle=True).history
+    model = compileModel(model, optimizer)
 
-    model.save('kerasNextWordModel.h5')
-    pickle.dump(history, open("history.p", "wb"))
-    model = load_model('kerasNextWordModel.h5')
-    history = pickle.load(open("history.p", "rb"))
+    model, history = trainModel(model, X, Y)
+
+    saveModel(model, history)
+
+    model, history = loadModel('kerasNextWordModel.h5', 'history.p')
 
     q = "Your life will never be the same again."
     print("Correct Sentence: ", q)
     seq = " ".join(RegexpTokenizer(r'\w+').tokenize(q.lower())[0:numPrevWords])
     print("Sequence: ", seq)
-    print("next possible words: ", predictCompletions(seq, model, uniqueWords, uniqueWordsIndex, numPrevWords, numSuggestions))
+    print("next possible words: ",
+          predictCompletions(seq, model, uniqueWords, uniqueWordsIndex, numPrevWords, numSuggestions))
 
 
 def getDataset(path):
@@ -89,12 +78,65 @@ def splitDataset(text):
     return words
 
 
+def preparePrevWords(data, numPrevWords):
+    previousWords = []
+    for i in range(len(data) - numPrevWords):
+        previousWords.append(data[i:i + numPrevWords])
+    return previousWords
+
+
+def prepareNextWords(data, numPrevWords):
+    nextWords = []
+    for i in range(len(data) - numPrevWords):
+        nextWords.append(data[i + numPrevWords])
+    return nextWords
+
+
+def prepareXYDatasets(previousWords, nextWords, numPrevWords, uniqueWords, uniqueWordsIndex):
+    X = np.zeros((len(previousWords), numPrevWords, len(uniqueWords)), dtype=bool)
+    Y = np.zeros((len(nextWords), len(uniqueWords)), dtype=bool)
+    for i, eachWords in enumerate(previousWords):
+        for j, eachWord in enumerate(eachWords):
+            X[i, j, uniqueWordsIndex[eachWord]] = 1
+        Y[i, uniqueWordsIndex[nextWords[i]]] = 1
+    return X, Y
+
+
+def prepareModel(numPrevWords, uniqueWords):
+    model = Sequential()
+    model.add(LSTM(128, input_shape=(numPrevWords, len(uniqueWords))))
+    model.add(Dense(len(uniqueWords)))
+    model.add(Activation('softmax'))
+    return model
+
+
 def prepareInput(text, numPreviousWords, uniqueWords, uniqueWordsIndex):
     X = np.zeros((1, numPreviousWords, len(uniqueWords)))
     for t, word in enumerate(text.split()):
         print(word)
         X[0, t, uniqueWordsIndex[word]] = 1
     return X
+
+
+def compileModel(model, optimizer):
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    return model
+
+
+def trainModel(model, X, Y):
+    history = model.fit(X, Y, validation_split=0.05, batch_size=128, epochs=epochs, shuffle=True).history
+    return model, history
+
+
+def saveModel(model, history):
+    model.save('kerasNextWordModel.h5')
+    pickle.dump(history, open("history.p", "wb"))
+
+
+def loadModel(modelPath, historyPath):
+    model = load_model(modelPath)
+    history = pickle.load(open(historyPath, "rb"))
+    return model, history
 
 
 def sample(preds, top_n):
@@ -113,8 +155,10 @@ def predictCompletions(text, model, uniqueWords, uniqueWordsIndex, numPreviousWo
     next_indicies = sample(preds, n)
     return [uniqueWords[idx] for idx in next_indicies]
 
-
 if __name__ == '__main__':
+    import os
+
+    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
     import tensorflow as tf
 
     with tf.device("/device:gpu:0"):
