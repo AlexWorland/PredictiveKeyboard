af0162da961649c68bea62dbfbfb914e96ca06ee
alex <alexworland@gmail.com>
2020-11-01 14:03:18 -0800

    Added main() back to main method

--- PredictiveKeyboard.py
+++ PredictiveKeyboard.py
@@ -220,5 +220,4 @@ if __name__ == '__main__':
     gpus = tf.config.experimental.list_physical_devices('GPU')
     for gpu in gpus:
         print("Name:", gpu.name, "  Type:", gpu.device_type)
-
-
+    main()
